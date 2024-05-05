c44b46ed6ae6618d661ba8071b3930884a66e2af
TruongNgan6 <truongvokimngan13@gmail.com>
2024-05-05 14:51:21 +0700

    doctor2

--- app/roles/doctors/find-donors/page.tsx
+++ app/roles/doctors/find-donors/page.tsx
@@ -1,11 +1,21 @@
+import { UserNav } from "@/components/roles/doctor-avatar";
+import { DataTableToolbar } from "@/components/roles/doctor-toolbar";
 import { PageTitle } from "@/components/roles/page-title";
+import { TableFindDonors } from "@/components/roles/table-doctors";
 
 export default function FindDonorsPage() {
-    return (
-        <>
-            <div>
-                <PageTitle title={"Find Donors"} className={""} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex">
+        <div className="grow h-14">
+          <PageTitle title={"Find Donors"} className={""} />
+        </div>
+        <div>
+          <UserNav />
+        </div>
+      </div>
+      <DataTableToolbar />
+      <TableFindDonors />
+    </>
+  );
 }
